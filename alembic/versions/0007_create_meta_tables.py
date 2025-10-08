28fe0fd99320dae944a02e42474c3843fb8294cf
1JigSaW <jigsaw11211@gmail.com>
2025-10-08 23:46:25 +0300

    Refactor category, size, and tag table definitions by removing unnecessary index creation, simplifying the schema for better clarity and performance.

--- alembic/versions/0007_create_meta_tables.py
+++ alembic/versions/0007_create_meta_tables.py
@@ -12,31 +12,25 @@ def upgrade() -> None:
     op.create_table(
         "categories",
         sa.Column("id", sa.BigInteger(), primary_key=True),
-        sa.Column("name", sa.String(length=50), unique=True, index=True, nullable=False),
+        sa.Column("name", sa.String(length=50), unique=True, nullable=False),
     )
-    op.create_index("ix_categories_name", "categories", ["name"], unique=True)
 
     op.create_table(
         "sizes",
         sa.Column("id", sa.BigInteger(), primary_key=True),
-        sa.Column("name", sa.String(length=16), unique=True, index=True, nullable=False),
+        sa.Column("name", sa.String(length=16), unique=True, nullable=False),
     )
-    op.create_index("ix_sizes_name", "sizes", ["name"], unique=True)
 
     op.create_table(
         "tags",
         sa.Column("id", sa.BigInteger(), primary_key=True),
-        sa.Column("name", sa.String(length=50), unique=True, index=True, nullable=False),
+        sa.Column("name", sa.String(length=50), unique=True, nullable=False),
     )
-    op.create_index("ix_tags_name", "tags", ["name"], unique=True)
 
 
 def downgrade() -> None:
-    op.drop_index("ix_tags_name", table_name="tags")
     op.drop_table("tags")
-    op.drop_index("ix_sizes_name", table_name="sizes")
     op.drop_table("sizes")
-    op.drop_index("ix_categories_name", table_name="categories")
     op.drop_table("categories")
 
 
